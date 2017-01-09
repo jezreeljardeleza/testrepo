100bdccf54a9d4a198230f52f1d4619c58e04114
jezreel <jezreeljardeleza@gmail.com>
2017-01-08 19:52:03 -0500

    removed app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-<!-- angular content will go here -->
-<<<<<<< HEAD
-<!-- this is a test --> 
-=======
-
-
-<!--    this should be a jezreel specific comment from jezreel branch   
- -->
->>>>>>> jezreel
